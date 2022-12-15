ce4a3f210eea18f6f4aca18ec925c19201076f5b
recep ozmen <recepozmen@gmail.com>
2022-12-16 00:57:40 +0300

    Day2: Hello World

---
+++ day02_HelloWorld/HelloCydeo.java
@@ -0,0 +1,20 @@
+package day02_HelloWorld;
+
+public class HelloCydeo {
+
+    public static void main (String[] kfc) {
+
+        System.out.println("Hello Cydeo");
+        System.out.println("JDK");
+        System.out.println("JRE");
+        System.out.println("JVM");
+
+
+    }
+
+
+
+
+
+
+}
